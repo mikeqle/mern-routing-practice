963fab49b33b034b81253063300d3598f3c17e81
Mike Le <mike.le@live.com>
2021-03-13 16:35:00 -0800

    completed assignment

---
+++ src/components/Display.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Display = (props) => {
+    const optionalStyle = props.optionalStyle.split("/");
+
+    console.log(optionalStyle);
+    const pStyle = {
+        color: optionalStyle.length === 2? optionalStyle[0]:null,
+        backgroundColor: optionalStyle.length === 2? optionalStyle[1]:null
+    };
+    
+
+    console.log(pStyle);
+
+    return(
+        <div>
+            {
+                isNaN(+ props.content) ?
+                <p style={pStyle}>The word is: { props.content }</p> :
+                <p style={pStyle}>The number is: { props.content }</p>
+            }
+        </div>
+    );
+}
+
+export default Display;
